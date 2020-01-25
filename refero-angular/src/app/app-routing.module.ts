4e27ccddd5f7ade3b458218d3f067716fd74c7f8
Garrett Woodall <garrett1woodall@gmail.com>
2020-01-25 14:37:10 -0600

    admin views

--- refero-angular/src/app/app-routing.module.ts
+++ refero-angular/src/app/app-routing.module.ts
@@ -4,15 +4,20 @@ import { LoginComponent } from "./login/login.component";
 import { RegistrationComponent } from "./registration/registration.component";
 import { GroupsComponent } from "./groups/groups.component";
 import { ListsPageComponent } from "./lists-page/lists-page.component";
-
 import { AdminHomeComponent } from "./admin-home/admin-home.component";
+import { BannedUsersComponent } from "./banned-users/banned-users.component";
+import { AllUsersComponent } from "./all-users/all-users.component";
+import { AllListsComponent } from "./all-lists/all-lists.component";
 
-const routes: Routes = [{ path: "admin-home", component: AdminHomeComponent }];
 const routes: Routes = [
   { path: "my-lists", component: ListsPageComponent },
   { path: "", component: LoginComponent },
   { path: "register", component: RegistrationComponent },
-  { path: "my-groups", component: GroupsComponent }
+  { path: "my-groups", component: GroupsComponent },
+  { path: "admin-home", component: AdminHomeComponent },
+  { path: "admin-home/banned-users", component: BannedUsersComponent },
+  { path: "admin-home/all-users", component: AllUsersComponent },
+  { path: "admin-home/all-lists", component: AllListsComponent }
 ];
 
 @NgModule({
