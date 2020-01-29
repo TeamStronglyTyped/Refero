d24e096d0af9ba08819a3215a5f70ff8058c5042
Remedee <zacharydanieldowns@gmail.com>
2020-01-29 09:03:36 -0600

    Broken Lists

--- refero-spring/src/main/java/com/referospring/service/ListItemsServiceImpl.java
+++ refero-spring/src/main/java/com/referospring/service/ListItemsServiceImpl.java
@@ -9,7 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class ListItemsServiceLogic implements ListItemsService {
+public class ListItemsServiceImpl implements ListItemsService {
 
     @Autowired
     private ListItemRepository listItemRepository;
--- refero-spring/src/main/java/com/referospring/service/ListsServiceImpl.java
+++ refero-spring/src/main/java/com/referospring/service/ListsServiceImpl.java
@@ -9,7 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class ListsServiceLogic implements ListsService {
+public class ListsServiceImpl implements ListsService {
 
     @Autowired
     private ListRepository listRepository;
