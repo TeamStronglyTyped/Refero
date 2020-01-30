7fd3fc7a333f7514043e8ef775ad10a2e352bd09
Garrett Woodall <garrett1woodall@gmail.com>
2020-01-30 13:57:46 -0600

    created list service in angular

---
+++ refero-angular/src/app/list.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { Lists } from "./models/lists";
+import { ListItems } from "./models/listItems";
+
+@Injectable({
+  providedIn: "root"
+})
+export class ListService {
+  private url: string;
+  constructor(private http: HttpClient) {
+    this.url = "http://localhost:5050";
+  }
+  public getAllLists(): Observable<Lists[]> {
+    return this.http.get<Lists[]>(this.url + "/get-all-lists");
+  }
+  public getListItems(): Observable<ListItems[]> {
+    return this.http.get<ListItems[]>(this.url + "/get-list-items");
+  }
+  public addList(list: Lists) {
+    return this.http.post<Lists>(this.url + "/post-new-list", list);
+  }
+  public delteList(id: number) {
+    return this.http.delete<Lists>(this.url + "/delte-list" + id);
+  }
+}
