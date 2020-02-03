2de21c2c40976ef5ec4549a8080443d5193973a8
Justin <justinsiegel17@gmail.com>
2020-02-03 17:41:54 -0600

    added tests to ensure users are invited and groups have names

--- refero-angular/src/app/create-group/create-group.component.spec.ts
+++ refero-angular/src/app/create-group/create-group.component.spec.ts
@@ -1,10 +1,9 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
-
-
 import { CreateGroupComponent } from './create-group.component';
 import { HttpClientModule } from '@angular/common/http'; 
+import { GroupSideNavComponent } from '../group-side-nav/group-side-nav.component';
+import { RouterTestingModule } from '@angular/router/testing';
 
 
 
@@ -14,9 +13,8 @@ describe('CreateGroupComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CreateGroupComponent ],
-      schemas : [ NO_ERRORS_SCHEMA ],
-      imports: [HttpClientTestingModule]
+      declarations: [ CreateGroupComponent, GroupSideNavComponent ],
+      imports: [HttpClientTestingModule, RouterTestingModule.withRoutes([]) ]
 
     })
     .compileComponents();
@@ -34,5 +32,27 @@ describe('CreateGroupComponent', () => {
       expect( component.isInvitedUserEmpty( invitedUser ) ).toBe( true );
     });
   } );
+
+  describe( 'isInvitedUserEmpty', () => {
+    it('should return false because there is an invited user', () => {
+      let invitedUser = 'testUser';
+      expect( component.isInvitedUserEmpty( invitedUser ) ).toBe( false );
+    });
+  } );
+
+  describe( 'isGroupNameEmpty', () => {
+    it('should return true because there is no group name', () => {
+      let groupName = '';
+      expect( component.isGroupNameEmpty( groupName ) ).toBe( true );
+    });
+  } );
+
+  describe( 'isGroupNameEmpty', () => {
+    it('should return false because there is a group name provided', () => {
+      let groupName = 'Dog';
+      expect( component.isGroupNameEmpty( groupName ) ).toBe( false );
+    });
+  } );
+
 
 });
