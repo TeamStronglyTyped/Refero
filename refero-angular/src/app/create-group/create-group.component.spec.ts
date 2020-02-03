e1e2f88061373ac141a28c1a8b880939b471d96f
Justin <justinsiegel17@gmail.com>
2020-02-03 17:26:10 -0600

    first test for create group component

--- refero-angular/src/app/create-group/create-group.component.spec.ts
+++ refero-angular/src/app/create-group/create-group.component.spec.ts
@@ -1,6 +1,12 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+
 
 import { CreateGroupComponent } from './create-group.component';
+import { HttpClientModule } from '@angular/common/http'; 
+
+
 
 describe('CreateGroupComponent', () => {
   let component: CreateGroupComponent;
@@ -8,7 +14,10 @@ describe('CreateGroupComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CreateGroupComponent ]
+      declarations: [ CreateGroupComponent ],
+      schemas : [ NO_ERRORS_SCHEMA ],
+      imports: [HttpClientTestingModule]
+
     })
     .compileComponents();
   }));
@@ -18,5 +27,12 @@ describe('CreateGroupComponent', () => {
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
+
+  describe( 'isInvitedUserEmpty', () => {
+    it('should return true because invited user is empty', () => {
+      let invitedUser = '';
+      expect( component.isInvitedUserEmpty( invitedUser ) ).toBe( true );
+    });
+  } );
 
 });
