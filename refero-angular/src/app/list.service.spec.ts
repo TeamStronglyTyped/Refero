38a5f0cfc58a2ed5935327a1d1966cdd9437e02c
Remedee <zacharydanieldowns@gmail.com>
2020-02-02 19:49:25 -0600

    Angular ListService Tests

--- refero-angular/src/app/list.service.spec.ts
+++ refero-angular/src/app/list.service.spec.ts
@@ -1,15 +1,57 @@
 import { TestBed, inject } from "@angular/core/testing";
-
 import { ListService } from "./list.service";
+import { Lists } from './models/lists';
+import { HttpClientModule } from '@angular/common/http';
 
 describe("ListService", () => {
   let listService: ListService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [HttpClientModule],
       providers: [ListService]
     });
 
     listService = TestBed.get(ListService);
+  });
+
+  describe('validList', () => {
+    it('should return false because of invalid listName', () => {
+      let list: Lists = new Lists;
+      list.listName = null;
+      list.group = 5;
+      list.owner = "someUser";
+      expect(listService.validList(list)).toBe(false);
+    });
+  });
+
+  describe('validList', () => {
+    it('should return false because of invalid group', () => {
+      let list: Lists = new Lists;
+      list.listName = "someList";
+      list.group = null;
+      list.owner = "someUser";
+      expect(listService.validList(list)).toBe(false);
+    });
+  });
+
+  describe('validList', () => {
+    it('should return false because of invalid owner', () => {
+      let list: Lists = new Lists;
+      list.listName = "someList";
+      list.group = 5;
+      list.owner = null;
+      expect(listService.validList(list)).toBe(false);
+    });
+  });
+
+  describe('validList', () => {
+    it('should return true because of valid list', () => {
+      let list: Lists = new Lists;
+      list.listName = "someList";
+      list.group = 5;
+      list.owner = "someOwner";
+      expect(listService.validList(list)).toBe(true);
+    });
   });
 });
--- refero-angular/src/app/list.service.ts
+++ refero-angular/src/app/list.service.ts
@@ -23,11 +23,23 @@ export class ListService {
   }
 
   public addList(list: Lists) {
-    return this.http.post<Lists>(this.url + "/post-new-list", list);
+    if (this.validList(list)) {
+      return this.http.post<Lists>(this.url + "/post-new-list", list);
+    }
   }
 
   public delteList(id: number) {
     return this.http.delete<Lists>(this.url + "/delte-list" + id);
+  }
+
+  public validList(list: Lists): boolean {
+    if (!list.listName || !list.group || !list.owner) {
+      return false;
+    }
+    if (list.group < 0) {
+      return false;
+    }
+    return true;
   }
 
 }
