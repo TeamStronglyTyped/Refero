b32bb8197e2b2493d16277771fb6c2627a5beb2b
Remedee <zacharydanieldowns@gmail.com>
2020-02-03 16:11:46 -0600

    Changes toward working ng build

--- refero-angular/src/app/banned-users/banned-users.component.ts
+++ refero-angular/src/app/banned-users/banned-users.component.ts
@@ -8,7 +8,7 @@ import { UsersService } from "../users.service";
   styleUrls: ["./banned-users.component.css"]
 })
 export class BannedUsersComponent implements OnInit {
-  private users: Users[];
+  public users: Users[];
   constructor(private userService: UsersService) {}
 
   ngOnInit() {
